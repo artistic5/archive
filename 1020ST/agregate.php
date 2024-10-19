1d5a0659eb1fb71d2ade87805e5255c011993661
artistic5 <artisticsoftware5@gmail.com>
2024-10-19 04:20:10 +0000

    ...

--- 1020ST/agregate.php
+++ 1020ST/agregate.php
@@ -11,30 +11,31 @@ return [
 		/**
 		Race 2
 		*/
-		'favorites' => '(F) 2, 7',
+		'favorites' => '(F) 2, 7, 10',
+		'win(union 2053)' => '2, 7, 10, 12',
+		'unions($10)' => '2, 4, 7, 10, 12',
+		'experimental($10)' => '2, 4, 7, 10, 12',
+		'win(union 2197)' => '2, 4, 7, 10',
 	],
 	'3' => [
 		/**
 		Race 3
 		*/
-		'favorites' => '(F) 4, 5',
+		'favorites' => '(F) 2, 4, 5, 8',
 	],
 	'4' => [
 		/**
 		Race 4
 		*/
-		'favorites' => '(F) 2, 11',
+		'favorites' => '(F) 1, 2, 3, 11',
 	],
 	'5' => [
 		/**
 		Race 5
 		*/
-		'favorites' => '(F) 5, 6',
-		'win(union )' => '1, 4, 5, 6',
+		'favorites' => '(F) 5, 6, 8, 10',
 		'unions($10)' => '1, 4, 5, 6',
 		'experimental($10)' => '1, 4, 5, 6',
-		'win(union 19)' => '4, 5, 6',
-		'win(union 567)' => '1, 5, 6',
 	],
 	'6' => [
 		/**
@@ -46,7 +47,13 @@ return [
 		/**
 		Race 7
 		*/
-		'favorites' => '(F) 2',
+		'favorites' => '(F) 2, 8',
+		'place(wp 451)' => '8',
+		'placesWP($30)' => '8, 2',
+		'win(union 1096)' => '2, 7, 8',
+		'unions($10)' => '2, 7, 8',
+		'experimental($10)' => '2, 7, 8',
+		'place(wp 1349)' => '2',
 	],
 	'8' => [
 		/**
@@ -65,7 +72,6 @@ return [
 		Race 10
 		*/
 		'favorites' => '(F) 8, 11',
-		'win(union 26)' => '7, 8, 11',
 		'unions($10)' => '7, 8, 11',
 		'experimental($10)' => '7, 8, 11',
 	],
