906e04f43ac8b7795a27fc3ead9d9ce956ee6877
artistic5 <artisticsoftware5@gmail.com>
2024-10-19 04:14:45 +0000

    ...

---
+++ 1020ST/agregate.php
@@ -0,0 +1,72 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '(F) 3, 5',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '(F) 2, 7',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '(F) 4, 5',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '(F) 2, 11',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '(F) 5, 6',
+		'win(union )' => '1, 4, 5, 6',
+		'unions($10)' => '1, 4, 5, 6',
+		'experimental($10)' => '1, 4, 5, 6',
+		'win(union 19)' => '4, 5, 6',
+		'win(union 567)' => '1, 5, 6',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '(F) 2, 4',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '(F) 2',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '(F) 2, 5',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '(F) 6',
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'favorites' => '(F) 8, 11',
+		'win(union 26)' => '7, 8, 11',
+		'unions($10)' => '7, 8, 11',
+		'experimental($10)' => '7, 8, 11',
+	],
+];
